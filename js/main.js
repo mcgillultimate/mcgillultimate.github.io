e9d4a7713cd83e57d027a047ffa2a58a5350081f
Edward <TuOfTimes@github.com>
2018-07-26 13:55:24 -0400

    added navbar and smooth scrolling

---
+++ js/main.js
@@ -0,0 +1,40 @@
+//Animate Smooth Scroll
+$('#view-about').on('click', function(){
+    const about = $('#about').position().top;
+
+    $('html, body').animate({
+        scrollTop: about
+    }, 900)
+});
+
+$('#view-roster').on('click', function(){
+    const roster = $('#roster').position().top;
+
+    $('html, body').animate({
+        scrollTop: roster
+    }, 900)
+});
+
+$('#view-uandu').on('click', function(){
+    const uandu = $('#uandu').position().top;
+
+    $('html, body').animate({
+        scrollTop: uandu
+    }, 900)
+});
+
+$('#view-sponsors').on('click', function(){
+    const sponsors = $('#sponsors').position().top;
+
+    $('html, body').animate({
+        scrollTop: sponsors
+    }, 900)
+});
+
+$('#view-contact').on('click', function(){
+    const contact = $('#contact').position().top;
+
+    $('html, body').animate({
+        scrollTop: contact
+    }, 900)
+});
