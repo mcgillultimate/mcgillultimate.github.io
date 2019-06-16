50b4672b16a58f16ed8b5faa699501e4bc62cb2f
Edward <TuOfTimes@github.com>
2019-06-16 00:06:49 -0400

    updated roster display section

--- js/main.js
+++ js/main.js
@@ -44,134 +44,60 @@ $('#roster-nav a').click(function() {
     return(false);   // no default behavior from clicking on the link
 });
 
-// Load 2016 rosters
-$('#view-2016').on('click', function(){
-    const mensA = ["Albert Kragl","Alistair Russell (C)","Angus Boswell","Arthur Denefle","Brenden Kadota","Christophe Tremblay-Joncas","Connor Fowlie","Daniel Tessier","Eamonn Lye","Edouard Levesque","Edward Tu","Elias Hess-Childs","Gabriel Russell","Henry Lu","Jason Galet","Kirk Wright","Lloyd Park","Lucas Ethgen","Neil Wu","Nicolas Swan","Paul Hooley","Simon Reynders","Thomas McAlear (C)","Thomas Ronson","Timmy Woo"]
-    const womensA = ["Amy Ker","Amy Song","Celine Huang","Claire Reierson","Daisy Lin (C)","Elsa Chu","Fanny Ma","Hannah Taylor","Helen Thompson (C)","Jacqueline Chi","Jenna Quong","Julia Biris","Katie Hu","Lily Zhu","Louise Podmore","Mariah Chang","Martha Schwarz","Melody Kwong","Noelia Gravotta","Sammie Lo"]
-    const mensB = ["Angus McLean","Ben Wong","Dylan Andrew Moran","Georges Cote","Harrison Kim","Jackson Foellmer","Josh Levin","Luca Stabile","Ludovic Cao","Michael Kleinman","Nathan Osman","Nic Levy","Nico Rullmann","Nicolas Vendeville","Odin Guo","Ryan Burgett","Thomas Bertrand","Tony To","William Maccaul"]
-    const womensB = ["Bailey Hardy","Carrie Gu","Chloe Rourke","Claire Amiro","Clara Chu","Danielle Findlay","Deifilia To","Inès Petard","Johanna Busch","Kavya Anchuri","Kelly Amrud","Lucia Wang","Mirela Maier","Neala MacLeod Farley","Nicole Pelletier","Paula Figueroa","Sierra Sewell","Zoe Yu"]
-    var players = "";
-    // Populate Men's A
-    for (var i = 0; i < mensA.length; i++) {
-        players += mensA[i] + "<br>";
-    }
-    document.getElementById("mens-A").innerHTML = players;
-    players = ""
-    // Populate Women's A
-    for (var i = 0; i < womensA.length; i++) {
-        players += womensA[i] + "<br>";
-    }
-    document.getElementById("womens-A").innerHTML = players;
-    players = ""
-    // Populate Men's B
-    for (var i = 0; i < mensB.length; i++) {
-        players += mensB[i] + "<br>";
-    }
-    document.getElementById("mens-B").innerHTML = players;
-    players = ""
-    // Populate Women's B
-    for (var i = 0; i < womensB.length; i++) {
-        players += womensB[i] + "<br>";
-    }
-    document.getElementById("womens-B").innerHTML = players;
-});
+function loadRoster(roster, practice_roster, captains, jersey_numbers) {
+    var table = document.getElementById('roster-table').getElementsByTagName('tbody')[0];
+    table.innerHTML = "";
+    // populate roster and numbers
+    for (var i = 0; i < roster.length; i++) {
+        var row = table.insertRow(-1);
+        var row_number = row.insertCell(0);
+        var row_name = row.insertCell(1);
 
-// Load 2017 rosters
-$('#view-2017').on('click', function(){
-    const mensA = ["Adam Gosselin","Albert Kragl","Brenden Kadota","Christophe Tremblay-Joncas","Christos Cunning","Daniel Tessier","Edward Tu","Elias Hess-Childs","Gabriel Gefter","Georges Cote","Henry Lu","Kirk Wright (C)","Lloyd Park","Lucas Ethgen","Ludovic Cao","Paul Hooley","Thomas McAlear (C)","Thomas Ronson","Thomas Wright","Timmy Woo (C)","Vincent Lee"]
-    const womensA = ["Amy Ker (C)","Bea Lehmann","Camille Forest","Celine Huang","Elsa Chu","Georgia Cato","Grace Longson","Ines Petard","Julia Biris (C)","Katie Hu (C)","Kelly Amrud","Lianne Campbell","Lindsay Potts","Lucia Wang","Mariah Chang","Paula Figueroa","Peggie Hsu","Taylor Johansen"]
-    const mensB = ["Alexander Li","Angus McLean","Dylan Andrew Golden","Émile Riel-Proulx","Everett Stamm","Finlay Douglas","Harrison Kim","Henry Richardson","Liam Baker","Jackson Spainhoward","Jay Jeong","Jesse Schwartz","Joey Richmond","Nathan Osman","Nico Rullman","Ryan Burgett","Saeesh Mangwani","Sam Hecht","Samuel Grégoire","Thibault Leyne","William MacCaul"]
-    const womensB = ["Clara Chu","Carrie Gu","Daria Eisner","Fabiana Chamis","Hanisah Hilmi","Helen Turvene","Jenna Yanke","Kaitlyn Liu","Lauren Benson-Armer","Lynda Au","Mirela Maier","Neala Farley","Nicole Pelletier","Noelia De La Cruz"]
-    var players = "";
-    // Populate Men's A
-    for (var i = 0; i < mensA.length; i++) {
-        players += mensA[i] + "<br>";
-    }
-    document.getElementById("mens-A").innerHTML = players;
-    players = ""
-    // Populate Women's A
-    for (var i = 0; i < womensA.length; i++) {
-        players += womensA[i] + "<br>";
-    }
-    document.getElementById("womens-A").innerHTML = players;
-    players = ""
-    // Populate Men's B
-    for (var i = 0; i < mensB.length; i++) {
-        players += mensB[i] + "<br>";
-    }
-    document.getElementById("mens-B").innerHTML = players;
-    players = ""
-    // Populate Women's B
-    for (var i = 0; i < womensB.length; i++) {
-        players += womensB[i] + "<br>";
-    }
-    document.getElementById("womens-B").innerHTML = players;
-});
+        row_number.innerHTML = jersey_numbers[i];
 
-// Load 2018 rosters
-$('#view-2018').on('click', function(){
-    const mensA = ["Adam Gosselin","Albert Kragl (C)","Angus Boswell","Brenden Kadota","Calvin Coulbury","Christophe Tremblay-Joncas (C)","Christos Cunning","Edward Tu","Elias Hess-Childs","Elliot Wong (PR)","Gabriel Gefter","Hayden Stone","Jack Doherty-Eagles","Jordan Faries","Paul Hooley","Thomas Griffin","Thomas McAlear (C)","Thomas Ronson","Thomas Wright","Timmy Woo (C)","Vincent Lee"]
-    const womensA = ["Amy Ker (C)","Alena Midgen","Antoinette Busch","Bea Lehmann","Caitlin Fowler (PR)","Camille Couture","Carrie Gu","Celine Huang","Cindy He","Deborah Su","Grace Longson","Helen Turvene","Ines Petard","Julia Biris","Kelly Amrud","Lianne Campbell","Lindsay Potts (C)","Lucia Wang","Lucy Core","Nina Tong","Paula Figeuroa"]
-    const mensB = ["Alexander Li","Adrien Philardeau-Planche","David Gao","David How","Derek Allen","Drake Wong","Elio Abi Younes","Émile Riel","Everett Stamm","Fenton Garvie","Fin Douglas","Guillaume Paquette","Henry Richardson","Jackson Foellmer","Jay Jeong","Liam Baker","Luca Stabile","Nathan Osman","Nikola Lemic","Ryan Burgett","Ryan Purkhart","Saeesh Mangwani","Sam Hecht","Samuel Grégoire","Theo Malhotra"]
-    const womensB = ["Haein Yoo","Jenna Yanke","Jill Miao","Katelyn Liu","Leonie Laug","Maria Castellanos","Milly Huang","Nicole Dacanay","Rachel Chen","Saphia Petard","Yasmine Bougueche"]
-    var players = "";
-    // Populate Men's A
-    for (var i = 0; i < mensA.length; i++) {
-        players += mensA[i] + "<br>";
+        if (practice_roster.indexOf(roster[i]) != -1){
+            row_name.innerHTML = "<i>" + roster[i] + "</i>";
+        }
+        else if (captains.indexOf(roster[i]) != -1) {
+            row_name.innerHTML = "<strong>" + roster[i] + "</strong>";
+        }
+        else {
+            row_name.innerHTML = roster[i];
+        }
     }
-    document.getElementById("mens-A").innerHTML = players;
-    players = ""
-    // Populate Women's A
-    for (var i = 0; i < womensA.length; i++) {
-        players += womensA[i] + "<br>";
-    }
-    document.getElementById("womens-A").innerHTML = players;
-    players = ""
-    // Populate Men's B
-    for (var i = 0; i < mensB.length; i++) {
-        players += mensB[i] + "<br>";
-    }
-    document.getElementById("mens-B").innerHTML = players;
-    players = ""
-    // Populate Women's B
-    for (var i = 0; i < womensB.length; i++) {
-        players += womensB[i] + "<br>";
-    }
-    document.getElementById("womens-B").innerHTML = players;  
-});
-
-
-
-function loadDefaultRosters() {
-    const mensA = ["Adam Gosselin","Albert Kragl (C)","Angus Boswell","Brenden Kadota","Calvin Coulbury","Christophe Tremblay-Joncas (C)","Christos Cunning","Edward Tu","Elias Hess-Childs","Elliot Wong (PR)","Gabriel Gefter","Hayden Stone","Jack Doherty-Eagles","Jordan Faries","Paul Hooley","Thomas Griffin","Thomas McAlear (C)","Thomas Ronson","Thomas Wright","Timmy Woo (C)","Vincent Lee"]
-    const womensA = ["Amy Ker (C)","Alena Midgen","Antoinette Busch","Bea Lehmann","Caitlin Fowler (PR)","Camille Couture","Carrie Gu","Celine Huang","Cindy He","Deborah Su","Grace Longson","Helen Turvene","Ines Petard","Julia Biris","Kelly Amrud","Lianne Campbell","Lindsay Potts (C)","Lucia Wang","Lucy Core","Nina Tong","Paula Figeuroa"]
-    const mensB = ["Alexander Li","Adrien Philardeau-Planche","David Gao","David How","Derek Allen","Drake Wong","Elio Abi Younes","Émile Riel","Everett Stamm","Fenton Garvie","Fin Douglas","Guillaume Paquette","Henry Richardson","Jackson Foellmer","Jay Jeong","Liam Baker","Luca Stabile","Nathan Osman","Nikola Lemic","Ryan Burgett","Ryan Purkhart","Saeesh Mangwani","Sam Hecht","Samuel Grégoire","Theo Malhotra"]
-    const womensB = ["Haein Yoo","Jenna Yanke","Jill Miao","Katelyn Liu","Leonie Laug","Maria Castellanos","Milly Huang","Nicole Dacanay","Rachel Chen","Saphia Petard","Yasmine Bougueche"]
-    var players = "";
-    // Populate Men's A
-    for (var i = 0; i < mensA.length; i++) {
-        players += mensA[i] + "<br>";
-    }
-    document.getElementById("mens-A").innerHTML = players;
-    players = ""
-    // Populate Women's A
-    for (var i = 0; i < womensA.length; i++) {
-        players += womensA[i] + "<br>";
-    }
-    document.getElementById("womens-A").innerHTML = players;
-    players = ""
-    // Populate Men's B
-    for (var i = 0; i < mensB.length; i++) {
-        players += mensB[i] + "<br>";
-    }
-    document.getElementById("mens-B").innerHTML = players;
-    players = ""
-    // Populate Women's B
-    for (var i = 0; i < womensB.length; i++) {
-        players += womensB[i] + "<br>";
-    }
-    document.getElementById("womens-B").innerHTML = players;  
 }
+
+const ma_roster = ["Edward Tu","Thomas Griffin","Thomas McAlear","Paul Hooley","Elias Hess-Childs","Gabriel Gefter","Hayden Stone","Christos Cunning","Thomas Ronson","Vincent Lee","Thomas Wright","Jack Eagles","Jordan Faries","Brenden Kadota","Calvin Coulbury","Christophe Tremblay-Joncas","Albert Kragl","Angus Boswell","Adam Gosselin","Elliot Wong","Timmy Woo"];
+const ma_practice_roster = ["Elliot Wong"];
+const ma_captains = ["Albert Kragl", "Thomas McAlear", "Christophe Tremblay-Joncas", "Timmy Woo"];
+const ma_jersey_numbers = [2,3,4,6,7,8,14,15,23,24,26,27,28,33,37,44,47,50,50,60,71];
+
+const wa_roster = ["Antoinette Busch","Ines Petard","Carrie Gu","Camille Couture","Helen Turvene","Julia Biris","Paula Figeuroa ","Deborah Su","Amy Ker","Grace Longson","Kelly Amrud","Cindy He","Lianne Campbell","Celine Huang","Lindsay Potts","Lucia Wang","Lucy Core","Jingjing Tong","Bea Lehmann","Caitlin Fowler"];
+const wa_practice_roster = ["Caitlin Fowler"];
+const wa_captains = ["Lindsay Potts", "Amy Ker"];
+const wa_jersey_numbers = [3,5,6,7,9,10,11,12,14,16,17,18,19,22,23,27,36,66,77,"NA"];
+
+const mb_roster = ["Nikola Lemic","Luca Fournier","Ryan Purkhart","Derek Allen","Jay Jeong","Liam Baker","Nathan Osman","Everett Stamm","Drake Wong","Elio Abi Younes","Henry Richardson","David Gao","Ryan Burgett","Jackson Foellmer","David How","Samuel Grégoire","Alexander Li ","Theo Malholtra","Fenton Garvie","Sam Hecht","Fin Douglas"];
+const mb_practice_roster = [];
+const mb_captains = [];
+const mb_jersey_numbers = [0,2,3,5,6,9,9,10,11,12,14,15,17,22,35,36,49,51,84,97,99];
+
+const wb_roster = ["Yasmine Bougueche","Saphia Petard","Haein Yoo","Rachel Chen","Katelyn Liu","Maria Castellanos","Jenna Yanke","Jill (Jingling) Miao","Leonie Laug","Milly Huang","Nicole Dacanay"];
+const wb_practice_roster = [];
+const wb_captains = [];
+const wb_jersey_numbers = [0,4,5,8,12,13,16,17,24,26,27];
+
+// Load Men's A rosters
+$('#view-ma').on('click', () => loadRoster(ma_roster, ma_practice_roster, ma_captains, ma_jersey_numbers));
+
+// Load Men's B rosters
+$('#view-mb').on('click', () => loadRoster(mb_roster, mb_practice_roster, mb_captains, mb_jersey_numbers));
+
+// Load Women's A rosters
+$('#view-wa').on('click', () => loadRoster(wa_roster, wa_practice_roster, wa_captains, wa_jersey_numbers));
+
+// Load Women's B rosters
+$('#view-wb').on('click', () => loadRoster(wb_roster, wb_practice_roster, wb_captains, wb_jersey_numbers));
 
 //Expand navbar
 $('#expand-nav').on('click', function(){
